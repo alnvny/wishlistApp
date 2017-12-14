9d980fc43cb338e342f1c79b9c02270b82e86f02
Gnana Allan Whinney G <gnana.gnanapragasam@viasat.com>
2017-12-14 15:54:21 +0530

    (WLA -2) changed env varialbe

--- app.js
+++ app.js
@@ -2,5 +2,6 @@ var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/public')); //__dir and not _dir
 var port = 8000; // you can use any port
-app.listen(port);
-console.log('server on' + port);
+app.listen(process.env.PORT || 5000, function() {
+    console.log('app is running at :' + process.env.PORT);
+});
